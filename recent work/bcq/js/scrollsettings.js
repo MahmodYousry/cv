e0b4e2a63fda7f393d2a01252671068e158e7de6
MahmodYousry <Yousry.mohmmed@rocketmail.com>
2020-08-09 14:42:42 +0200

    All cv files added

---
+++ recent work/bcq/js/scrollsettings.js
@@ -0,0 +1,57 @@
+/*global $, jQuery , alert */
+$(document).ready(function () {
+    
+    "use strict";
+    
+	//Nice Scroll
+	
+	
+	//$("html").niceScroll({
+	//	cursorcolor:"#E41b17",
+	//	cursorwidth:"10px",
+	//	background:"rgba(20,20,20,0.3)",
+	//	cursorborder:"1px solid #E41b17",
+	//	cursorborderradius:0
+	//	});
+		
+	$("").niceScroll({
+		ursorwidth:12,
+		cursoropacitymin:0.4,
+		cursorcolor:'#6e8cb6',
+		cursorborder:'none',
+		cursorborderradius:4,
+		autohidemode:'leave'
+	});
+		
+	$("").niceScroll({
+		cursorwidth:12,
+		cursoropacitymin:0.4,
+		cursorcolor:'#6e8cb6',
+		cursorborder:'none',
+		cursorborderradius:4,
+		autohidemode:'leave'
+	});  // free your immagination
+		
+	// $("#sdsd").niceScroll({
+	//	cursorcolor:"#E41b17",
+	//	cursorwidth:"10px",
+	//	background:"rgba(20,20,20,0.3)",
+	//	cursorborder:"0px solid #E41b17",
+	//	cursorborderradius:0
+	//		});
+
+
+
+    
+ 
+});
+
+
+
+
+
+
+
+
+
+
